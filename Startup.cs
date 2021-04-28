d8f596b6c861d0c8219e46e6cd571403400a9bef
GoalPanda <goalpanda111@gmail.com>
2021-04-28 05:15:53 -0700

    corse issue fix

--- Startup.cs
+++ Startup.cs
@@ -84,7 +84,7 @@ namespace TimeTracker_server
 
       app.UseCors(x => x
         .AllowAnyOrigin()
-        .AllowAnyMethod()
+        .WithMethods("GET", "PUT", "DELETE", "POST", "OPTIONS")
         .AllowAnyHeader());
 
       // app.UseAuthentication();
